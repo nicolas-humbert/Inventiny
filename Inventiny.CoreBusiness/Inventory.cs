db9edf581c749bdcb1363c244d2092d7d577bf2e
Nicolas Humbert <nicolas.s.humbert@gmail.com>
2023-08-26 17:30:57 +0200

    Added view for AddInventory use case; Added form and logic validation for adding inventories;

--- Inventiny.CoreBusiness/Inventory.cs
+++ Inventiny.CoreBusiness/Inventory.cs
@@ -1,10 +1,18 @@
-﻿namespace Inventiny.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventiny.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required(ErrorMessage = "The inventory name is required")]
         public string InventoryName { get; set; } = String.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0.")]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0.")]
         public double Price { get; set; }
     }
 }
